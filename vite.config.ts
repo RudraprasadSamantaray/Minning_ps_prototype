a86c96ed30fde31d0e909faafaffa872e5dd77da
rudra <daitari12584@gmail.com>
2026-09-06 21:30:19 +0530

    Fix Render host configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview:{
+    host:'0.0.0.0',
+    allowedHosts: [' minning-ps-prototype.onrender.com'],
+  }
 })
