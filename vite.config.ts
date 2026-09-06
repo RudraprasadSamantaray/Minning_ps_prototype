b1b2a1fac6a3a7b12d9d61101db0e64c9ea4b123
rudra <daitari12584@gmail.com>
2026-09-06 21:34:33 +0530

    Fix allowedHosts configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview:{
-    host:'0.0.0.0',
-    allowedHosts: [' minning-ps-prototype.onrender.com'],
-  }
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: true,
+  },
 })
